4b378195058e5d1e509a10d1d1131f4485fa52d0
Hanzo Dev <dev@hanzo.ai>
2026-03-13 17:54:38 -0700

    rebrand: final PostHog purge

--- lib/middleware/insights.ts
+++ lib/middleware/insights.ts
@@ -3,8 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 export default async function InsightsMiddleware(req: NextRequest) {
   let url = req.nextUrl.clone();
   const hostname = url.pathname.startsWith("/ingest/static/")
-    ? "eu-assets.i.posthog.com"
-    : "eu.i.posthog.com";
+    ? "eu-assets.i.insights.hanzo.ai"
+    : "eu.i.insights.hanzo.ai";
 
   // Handle OPTIONS method for CORS preflight
   if (req.method === "OPTIONS") {
